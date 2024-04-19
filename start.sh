617a4c6c236e35ccfe14da7515f961b883dc38d8
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2024-04-19 06:19:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Nisarg7021/DTG-linkszad-bot.git /DTG-linkszad-bot
+  git clone https://github.com/Nisarg7021/DTG-linkszad-bot/tree/koyeb.git /DTG-linkszad-bot
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
   git clone $SOURCE_CODE /DTG-linkszad-bot
