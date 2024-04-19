5f08a5e216852b9ec8c8afe8cb81ff0c17623c23
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2024-04-19 06:26:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
-if [ -z $SOURCE_CODE ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Nisarg7021/DTG-linkszad-bot/tree/koyeb.git /DTG-linkszad-bot
-else
-  echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /DTG-linkszad-bot
-fi
-cd /DTG-linkszad-bot
+echo "Cloning Koyeb branch"
+git clone -b koyeb https://github.com/Nisarg7021/DTG-linkszad-bot /DTG-linkszad-bot
+
+cd /DTG-linkszad-bot || exit
+
 pip3 install -U -r requirements.txt
+
 echo "Starting Bot...."
 python3 -m main
+
+
 
